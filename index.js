7a395405e737aa3ff0c2dc99c2bb4229f8f0ed89
Jair Aya <54295211+jairayafranco@users.noreply.github.com>
2024-10-27 14:17:15 -0500

    Updated select options and show registers

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const baseUrl = 'http://localhost/totalcode_back/public';
 
 $('#loginForm').submit(function (e) {
-    e.preventDefault(); // Evita que el formulario se envíe de forma normal
+    e.preventDefault();
 
-    // Obtiene los valores del formulario
     const username = $('#username').val();
     const password = $('#password').val();
 
@@ -17,46 +16,106 @@ $('#loginForm').submit(function (e) {
         },
         success: function (data) {
             if (data.status) {
-                $('#loginMessage').text('Inicio de sesión exitoso').css('color', 'green');
-                // Redirigir o cargar contenido protegido
-
                 const token = data.token;
-                const expirationTime = Date.now() + 5 * 60 * 1000; // 5 minutos en milisegundos
-
-                // Guardar el token y el tiempo de expiración en localStorage
                 localStorage.setItem('auth_token', token);
-                localStorage.setItem('expirationTime', expirationTime);
 
                 $('.login-container').hide();
                 $('.table-container').show();
+
+                $('#username').val('');
+                $('#password').val('');
+                getOrders();
             } else {
                 $('#loginMessage').text('Usuario o contraseña incorrectos').css('color', '#d32f2f');
             }
         },
         error: function (e) {
             $('#loginMessage').text('Ocurrió un error en la autenticación').css('color', '#d32f2f');
-            console.log("error", e);
+            console.error("error>>>", e);
         }
     });
 });
 
 $(document).ready(function () {
-    // Comprobar si el token ha expirado al cargar la página
-    if (isTokenExpired()) {
-        // Token expirado, redirigir a la página de inicio de sesión o mostrar un mensaje
-        $('.login-container').show();
-        $('.table-container').hide();
-    } else {
-        // Token válido, puedes proceder con las operaciones que requieran autenticación
-        const token = sessionStorage.getItem('miToken');
-        console.log('Token válido:', token);
-        $('.login-container').hide();
-        $('.table-container').show();
-        // Aquí puedes hacer llamadas a tu API usando el token
-    }
+    isTokenExpired(); // Comprobar si el token ha expirado al cargar la página
 });
 
+
+$('.btn-salir').click(function () {
+    localStorage.removeItem('auth_token');
+    $('.login-container').show();
+    $('.table-container').hide();
+});
+
+function getOrders() {
+    $.ajax({
+        url: baseUrl + '/api/orders',
+        type: 'GET',
+        success: function (orders) {
+            if (orders) {
+                const tableBody = $('#tableBody');
+
+                orders.forEach(order => {
+                    const row = `<tr>
+                        <td>${order.first_name} ${order.last_name}<br /><small>${order.email}</small></td>
+                        <td>${order.order_num}</td>
+                        <td>${formatCurrency(order.total)}</td>
+                    </tr>`;
+                    tableBody.append(row);
+                });
+
+                const total = orders.reduce((acc, order) => acc + Number(order.total), 0);
+
+                const totalRow = `
+                    <tr class="total-row">
+                        <td>Total</td>
+                        <td>21</td>
+                        <td>${formatCurrency(total)}</td>
+                    </tr>`;
+                tableBody.append(totalRow);
+
+                $('.records').text(`Registros (${orders.length})`)
+            } else {
+                console.error('Error al obtener las órdenes');
+            }
+        },
+        error: function (e) {
+            console.error("error>>>", e);
+        }
+    })
+}
+
 function isTokenExpired() {
-    const expirationTime = localStorage.getItem('expirationTime');
-    return expirationTime ? Date.now() > expirationTime : true;
+    const token = localStorage.getItem('auth_token');
+
+    $.ajax({
+        url: baseUrl + '/api/auth/check',
+        type: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + token
+        },
+        success: function (data) {
+            if (!data.status) {
+                localStorage.removeItem('auth_token');
+                $('.login-container').show();
+                $('.table-container').hide();
+                return;
+            }
+
+            $('.login-container').hide();
+            $('.table-container').show();
+            getOrders();
+        },
+        error: function (e) {
+            console.error("error>>>", e);
+        }
+    });
+}
+
+function formatCurrency(value) {
+    return new Intl.NumberFormat('es-CO', {
+        style: 'currency',
+        currency: 'COP',
+        maximumFractionDigits: 0
+    }).format(value);
 }
